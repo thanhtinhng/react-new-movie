c3ce89c7a078af41f335d73042a9541b6a2036f7
Nguyen Thanh Tinh <ngthanhtinh801@gmail.com>
2024-11-01 12:39:42 +0700

    scss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className='global-style'>
+      <a href="#">test</a>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default App;
