8047546e3aa98fbc5f8febd49a59cecc85b6aa16
Nguyen Thanh Tinh <ngthanhtinh801@gmail.com>
2024-11-04 01:21:58 +0700

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import React from 'react';
 import './App.scss';
-import apiConfig from './api/apiConfig'
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import AppRoutes from './routes/Routes'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import { Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className='global-style'>
-      <a href="#">test</a>
-      <input type="text" />
+      <BrowserRouter>
+        <>
+          <Header />
+          <AppRoutes />
+          <Footer />
+        </>
+      </BrowserRouter>
     </div>
   );
 }
