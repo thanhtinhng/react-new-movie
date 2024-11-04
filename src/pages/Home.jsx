2b7f17c650ea675ee87f627d63c1e8a9608e0ef8
Thanh Tinh Nguyen <ngthanhtinh0801@gmail.com>
2024-11-04 12:34:05 +0700

    button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Button, {OutlineButton} from '../components/button/Button'
+import './Home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='test'>
+      Home
+      <Button>button</Button>
+      <OutlineButton>button</OutlineButton>
+    </div>
   )
 }
 
